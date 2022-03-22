b796291fba3ba358a5bc0a06068a9730748c3a19
Vlad <predgd@pm.me>
2022-03-22 11:53:42 +0100

    add buttons

---
+++ lib/widgets/buttons/button_row.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:subtrack/consts/colors.dart';
+import 'package:subtrack/consts/sizes.dart';
+import 'package:subtrack/widgets/buttons/custom_button.dart';
+
+class ButtonRow extends StatelessWidget {
+  const ButtonRow({
+    Key? key,
+    required this.leftText,
+    required this.rightText,
+    this.leftColor = Colors.white,
+    this.rightColor = Colors.white,
+    required this.callback,
+  }) : super(key: key);
+
+  final String leftText;
+  final String rightText;
+  final Color leftColor;
+  final Color rightColor;
+  final Function(bool val) callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Expanded(
+          child: CustomButton(
+            text: leftText,
+            color: leftColor,
+            callback: callback,
+            callbackValue: false,
+          ),
+        ),
+        Container(
+          height: BUTTON_HEIGHT - 20,
+          width: 1,
+          color: appBarColorMat,
+        ),
+        Expanded(
+          child: CustomButton(
+            text: rightText,
+            color: rightColor,
+            callback: callback,
+            callbackValue: true,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/buttons/custom_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:subtrack/consts/sizes.dart';
+import 'package:subtrack/consts/text_styles.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    Key? key,
+    required this.text,
+    this.color = Colors.white,
+    this.fontSize = BUTTON_TEXT_SIZE,
+    this.height = BUTTON_HEIGHT,
+    this.fontWeight = BUTTON_TEXT_WEIGHT,
+    required this.callback,
+    required this.callbackValue,
+  }) : super(key: key);
+
+  final String text;
+  final Color color;
+  final double fontSize;
+  final double height;
+  final FontWeight fontWeight;
+
+  final Function(bool val) callback;
+  final bool callbackValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        splashColor: color.withOpacity(0.5),
+        highlightColor: color.withOpacity(0.2),
+        borderRadius: BorderRadius.circular(BORDER_RADIUS),
+        onTap: () => callback(callbackValue),
+        child: SizedBox(
+          height: height,
+          child: Align(
+            child: Text(
+              text,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: color,
+                fontSize: fontSize,
+                fontWeight: fontWeight,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
