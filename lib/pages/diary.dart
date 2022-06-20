274ce61b5ede29be534ac5c57bf0065a992ce54c
Vlad <predgd@pm.me>
2022-06-20 23:28:46 +0200

    humble beginnings of the diary

--- lib/pages/diary.dart
+++ lib/pages/diary.dart
@@ -1,20 +1,17 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:subtrack/consts/colors.dart';
-import 'package:subtrack/consts/sizes.dart';
 
 class DiaryPage extends StatelessWidget {
   const DiaryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        decoration: BoxDecoration(
-          color: empathogenColorMat,
-          borderRadius: BorderRadius.circular(borderRadius),
+    return Scaffold(
+      body: Container(
+        decoration: linearBg,
+        child: const Center(
+          child: Text("pretend this is the diary page"),
         ),
-        height: 100,
-        width: 100,
       ),
     );
   }
