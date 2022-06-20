1f75791b29d812c6f0f31890c491e6a0fde63067
Vlad <predgd@pm.me>
2022-06-20 20:44:37 +0200

    read settings file from app dir

--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:subtrack/consts/strings.dart';
 import 'package:subtrack/data/settings_data.dart';
+import 'package:subtrack/managers/file_manager.dart';
 
 class Settings {
   static final Settings _settings = Settings._internal();
@@ -9,14 +13,17 @@ class Settings {
   SettingsData data = SettingsData();
 
   Future<void> readSettings() async {
-    // final String content = await FileManager().readFile(
-    //   path: FileManager().getRootAppDirPath,
-    //   fileName: settingsFileName,
-    // );
-    // if (content.isEmpty) {
-    //   await data.writeSettings();
-    // } else {
-    //   data = SettingsData.fromJson(jsonDecode(content) as Map<String, dynamic>);
-    // }
+    final String content = await FileManager().readFile(
+      path: FileManager().getAppDocDirPath,
+      fileName: settingsFileName,
+    );
+    if (content.isEmpty) {
+      await data.writeSettings();
+    } else {
+      data = SettingsData.fromJson(jsonDecode(content) as Map<String, dynamic>);
+      if (data.databasePath != null) {
+        FileManager().rootAppDirPath = data.databasePath!;
+      }
+    }
   }
 }
