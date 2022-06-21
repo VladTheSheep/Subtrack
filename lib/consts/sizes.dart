e167275913585cc7faa25ce457d97c5bae14b1e5
Vlad <predgd@pm.me>
2022-06-21 07:23:11 +0200

    reduce header text size

--- lib/consts/sizes.dart
+++ lib/consts/sizes.dart
@@ -2,7 +2,7 @@ const double borderRadius = 8.0;
 
 const double elevation = 5.0;
 
-const double headerTextSize = 20.0;
+const double headerTextSize = 18.0;
 const double buttonTextSize = 16.0;
 
 const double buttonHeight = 50.0;
